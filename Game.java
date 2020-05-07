e634aa5720cdd97afe39f6777a055b1ed77f22c8
christaphat <cmwest135@gmail.com>
2020-05-07 13:13:27 -0700

    Feature - added a new map ban feature

--- Game.java
+++ Game.java
@@ -2,9 +2,12 @@ import java.util.*;
 
 public class Game{
 
-	List<bigPlayer> lobby = new ArrayList<bigPlayer>();
-	List<bigPlayer> team1 = new ArrayList<bigPlayer>();
-	List<bigPlayer> team2 = new ArrayList<bigPlayer>();
+	List<Player> lobby = new ArrayList<Player>();
+	List<Player> team1 = new ArrayList<Player>();
+	List<Player> team2 = new ArrayList<Player>();
+	List<Map> mapList = new ArrayList<Map>();
+	List<Player> team1MapPickers = new ArrayList<Player>();
+	List<Player> team2MapPickers = new ArrayList<Player>();
 	static Scanner in = new Scanner(System.in);
 
 	public static void main(String[] args) throws notCaptainException{
@@ -22,39 +25,50 @@ public class Game{
 		}
 		System.out.println("The lobby is now full");
 		newGame.chooseCaptains(newGame.lobby);
-		newGame.captainChoice(newGame.team1.get(0), newGame.team2.get(0), newGame.lobby);		/*for(int j = 0; j<5; j++){
-			System.out.println("Team 1: " + newGame.team1.get(i).getName());
-			System.out.println("Team 2: " + newGame.team2.get(i).getName());
-		}*/
-		
-		/*System.out.println("lobby before: " + newGame.lobby.toString());
-		System.out.println("team1 before: " + newGame.team1.toString());
-		System.out.println("team2 before: " + newGame.team2.toString());
-		newGame.chooseCaptains(newGame.lobby);
-		System.out.println("team1 after picking captains: " + newGame.team1.toString());
-		System.out.println("team2 after picking captains: " + newGame.team2.toString());
-		newGame.captainChoice(newGame.team1.get(0), newGame.team2.get(0), newGame.lobby.get(0), newGame.lobby);
-		newGame.addPlayerToTeam(newGame.lobby.get(0), newGame.team1);
-		System.out.println("lobby after: " + newGame.lobby.toString());
-		System.out.println("team1 after: " + newGame.team1.toString());
-		System.out.println("team2 after: " + newGame.team2.toString());
-		for(int i = 0; i < 5; i++){
-			System.out.println("Team 1:" + newGame.team1.get(i).getName());
-			System.out.println("Team 2:" + newGame.team2.get(i).getName());
-			System.out.println(newGame.lobby.get(i).isCaptain);
-			System.out.println(newGame.lobby.get(i).toString());
-		}*/
+		newGame.captainPickPlayers(newGame.team1.get(0), newGame.team2.get(0), newGame.lobby);
+		System.out.println("Available Competitive Maps");
+		newGame.addMap("Mirage");
+		newGame.addMap("Inferno");
+		newGame.addMap("Overpass");
+		newGame.addMap("Nuke");
+		newGame.addMap("Train");
+		newGame.addMap("Dust2");
+		newGame.addMap("Cache");
+		for(int j = 0; j < newGame.mapList.size(); j++){
+			System.out.print(newGame.mapList.get(j).getMapName() + " ");
+		}
+		System.out.println();
+		//2 players from team 1
+		newGame.selectPlayersPickingMaps(newGame.team1, newGame.team1MapPickers);
+		//2 players from team 2
+		newGame.selectPlayersPickingMaps(newGame.team2, newGame.team2MapPickers);
+		System.out.println("These players will be picking the maps:");
+		System.out.println(newGame.team1MapPickers.get(0).getName() + " " +  newGame.team1MapPickers.get(1).getName());
+		System.out.println(newGame.team2MapPickers.get(0).getName() + " " +  newGame.team2MapPickers.get(1).getName());
+		//Players choosing maps to ban
+		newGame.mapBanning(newGame.team1MapPickers.get(0));
+		newGame.mapBanning(newGame.team2MapPickers.get(0));
+		newGame.mapBanning(newGame.team2MapPickers.get(1));
+		newGame.mapBanning(newGame.team1MapPickers.get(1));
+		System.out.println("These are the available maps after the ban:");
+		for(int j = 0; j < newGame.mapList.size(); j++){
+			System.out.print(newGame.mapList.get(j).getMapName() + " ");
+		}
+		Collections.shuffle(newGame.mapList);
+		for(int j = 0; j < newGame.mapList.size(); j++){
+			System.out.print(newGame.mapList.get(j).getMapName() + " ");
+		}
 	}
 
 	public Game(){
 	}
 
 	public void addPlayer(String playerName, boolean iscaptain, int id){
-		bigPlayer newPlayer = new bigPlayer(playerName, iscaptain, id);
+		Player newPlayer = new Player(playerName, iscaptain, id);
 		lobby.add(newPlayer);
 	}
 
-	public void removePlayer(bigPlayer player, List<bigPlayer> placement){
+	public void removePlayer(Player player, List<Player> placement){
 		int placementLength = placement.size();
 		int i = 0;
 		while(i < placementLength){
@@ -66,11 +80,57 @@ public class Game{
 		}
 	}
 
-	public void captainChoiceSecondary(HashSet<String> setOfPlayers, int captainNum){
+	public void addMap(String map){
+		Map newMap = new Map(map);
+		mapList.add(newMap);
+	}
+
+	public void selectPlayersPickingMaps(List<Player> team, List<Player> teamMapPickers){
+		Random rand = new Random();
+		int first = rand.nextInt(5);
+		teamMapPickers.add(team.get(first));
+		int second = rand.nextInt(5);
+		while(second == first){
+			second = rand.nextInt(5);
+		}
+		teamMapPickers.add(team.get(second));
+	}
+
+	public void mapBanning(Player playerSelectingMapToBan){
+		System.out.println(playerSelectingMapToBan.getName() + " - Please Select a map to ban from the following: ");
+		for(int j = 0; j < mapList.size(); j++){
+			System.out.print(mapList.get(j).getMapName() + " ");
+		}
+		System.out.println();
+		HashSet<String> setOfPlayers = new HashSet<String>();
+		for(int i = 0; i < mapList.size(); i++){
+			setOfPlayers.add(mapList.get(i).getMapName());
+		}
+		String s = in.nextLine();
+		boolean isIncluded = false;
+		while(isIncluded == false){
+			if(setOfPlayers.contains(s)){
+				isIncluded = true;
+				break;
+			}else{
+				System.out.print("Please enter a valid map name: ");
+				s = in.nextLine();
+			}
+		}
+		for(int i = 0; i < mapList.size(); i++){
+			if(mapList.get(i).getMapName().equals(s)){
+				System.out.println(mapList.get(i).getMapName() + " has been banned.");
+				mapList.remove(mapList.get(i));
+				break;
+			}
+		}
+	}
+
+	public void captainPickPlayersHelper(HashSet<String> setOfPlayers, int captainNum, List<Player> team){
 		for(int i = 0; i<lobby.size(); i++){
 			System.out.println(lobby.get(i).getName() + " - " + lobby.get(i).getId());
 		}
-		bigPlayer temp = new bigPlayer();
+		Player temp = new Player();
 		System.out.println("Captain " + captainNum + ", please choose a player using their name:");
 		String s = in.nextLine();
 		boolean isIncluded = false;
@@ -89,7 +149,7 @@ public class Game{
 				break;
 			}
 		}
-		addPlayerToTeam(temp, team1);
+		addPlayerToTeam(temp, team);
 		removePlayer(temp, lobby);
 		System.out.print("This is the current Lobby ");
 		for(int i = 0; i < lobby.size(); i++){
@@ -108,14 +168,14 @@ public class Game{
 		System.out.println();
 	}
 
-	public void captainChoice(bigPlayer captain1, bigPlayer captain2, List<bigPlayer> lobby) throws notCaptainException{
+	public void captainPickPlayers(Player captain1, Player captain2, List<Player> lobby) throws notCaptainException{
 		if(captain1.isCaptain == false){
 			throw new notCaptainException("this person is not a captain");
 		}
 		if(captain2.isCaptain == false){
 			throw new notCaptainException("this person is not a captain");
 		}
-		System.out.println("Captains are choosing teams");
+		System.out.println("Captains are choosing teams:");
 		boolean captain1Choice = true;
 		boolean captain2Pick2 = true;
 		boolean captain1Pick2 = false;
@@ -125,30 +185,30 @@ public class Game{
 				setOfPlayers.add(lobby.get(i).getName());
 			}
 			if(captain1Choice == true){
-				captainChoiceSecondary(setOfPlayers, 1);
+				captainPickPlayersHelper(setOfPlayers, 1, team1);
 				captain1Choice = false;
 			}if(captain1Pick2 == true){
-				captainChoiceSecondary(setOfPlayers, 1);
+				captainPickPlayersHelper(setOfPlayers, 1, team1);
 				captain1Choice = false;
 				captain1Pick2 = false;
 			}
 			if(captain1Choice == false){
-				captainChoiceSecondary(setOfPlayers, 2);
+				captainPickPlayersHelper(setOfPlayers, 2, team2);
 				captain1Choice = true;
 			}
 			if(captain2Pick2 == true){
-				captainChoiceSecondary(setOfPlayers, 2);
+				captainPickPlayersHelper(setOfPlayers, 2, team2);
 				captain2Pick2 = false;
 				captain1Pick2 = true;
 			}
 		}
 	}
 
-	public void addPlayerToTeam(bigPlayer player, List<bigPlayer> team){
+	public void addPlayerToTeam(Player player, List<Player> team){
 		team.add(player);
 	}
 
-	public void chooseCaptains(List<bigPlayer> lobby){
+	public void chooseCaptains(List<Player> lobby){
 		Random random = new Random();
 		int first, second = 0;
 		first = random.nextInt(10);
@@ -167,7 +227,7 @@ public class Game{
 		removePlayer(lobby.get(second), lobby);
 	}
 
-	public void passCaptain(bigPlayer currentCaptain, bigPlayer futureCaptain){
+	public void passCaptain(Player currentCaptain, Player futureCaptain){
 		currentCaptain.isCaptain = false;
 		futureCaptain.isCaptain = true;
 	}
@@ -195,15 +255,30 @@ public class Game{
 	}*/
 }
 
-class bigPlayer{
+class Map{
+	String mapName = "";
+
+	Map(){
+	}
+
+	Map(String map){
+		this.mapName = map;
+	}
+
+	public String getMapName(){
+		return this.mapName;
+	}
+}
+
+class Player{
 	String name = "";
 	boolean isCaptain = false;
 	int playerId = 0;
 
-	bigPlayer(){
+	Player(){
 	}
 
-	bigPlayer(String playerName, boolean captain, int id){
+	Player(String playerName, boolean captain, int id){
 		this.name = playerName;
 		this.isCaptain = captain;
 		this.playerId = id;
